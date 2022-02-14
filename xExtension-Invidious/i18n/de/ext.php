1a89fe296b003e4c6377e3ccb939ebc7b1bd5e77
Clemens Neubauer <cn-tools@gmx.at>
2022-02-14 16:23:12 +0100

    fix some problems
    * fix url to github
    * fix translation problem with german language

--- xExtension-Invidious/i18n/de/ext.php
+++ xExtension-Invidious/i18n/de/ext.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-	'yt_videos' => array(
+	'in_videos' => array(
 		'height' => 'Höhe des Players',
         'width' => 'Breite des Players',
         'updates' => 'Die neueste Version des Plugins findest Du bei',
